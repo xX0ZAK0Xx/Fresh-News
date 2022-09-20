a1b2e5249ff33b31764cc9285239ca9480ae5c35
Zayed_Oyshik <zayedoyshik1@gmail.com>
2022-09-20 19:54:33 +0600

    base recycler added

--- app/src/main/java/com/example/freshnews/MainActivity.kt
+++ app/src/main/java/com/example/freshnews/MainActivity.kt
@@ -2,10 +2,32 @@ package com.example.freshnews
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), NewsItemClicked {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val area : RecyclerView = findViewById<RecyclerView>(R.id.newsplace)
+        area.layoutManager = LinearLayoutManager(this)
+        val items = fetchdata()
+        val adapter = NewsListAdapter(items, this)
+        area.adapter = adapter
     }
+    private fun fetchdata(): ArrayList<String>{
+        val list = ArrayList<String>()
+        for(i in 0 until 100){
+            list.add("Item $i")
+        }
+        return  list
+    }
+
+    override fun onItemClicked(item: String) {
+        Toast.makeText(this,"Clicked item $item", Toast.LENGTH_LONG).show()
+    }
+
 }
